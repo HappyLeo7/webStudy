3d65dcce265186363f6d5fa7d17f85de65fa6892
HappyLeo7 <suh0007@naver.com>
2025-09-12 09:38:30 +0900

    mybatis세팅수정

--- 2025_0911_MybatisTest/src/main/java/service/MyBatisConnector.java
+++ 2025_0911_MybatisTest/src/main/java/service/MyBatisConnector.java
@@ -3,8 +3,10 @@ package service;
 import java.io.IOException;
 import java.io.Reader;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 
 
 public class MyBatisConnector {
